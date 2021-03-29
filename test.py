4e5dce8d262c47cc3124c203f5c2b220cf567856
Vũ Trần Tiến Bảo <vutrantienbao290699@gmail.com>
2021-03-29 14:38:11 +0700

    commit lan 1

--- test.py
+++ test.py
@@ -6,31 +6,39 @@ import json
 import wiotp.sdk.application
 import dns
 from pymongo import MongoClient
+from flask import Flask
 # Khởi tạo một kết nối
 
-myConfig = {
+app = Flask(__name__)
+@app.route("/")
+def main():
+    myConfig = {
         "auth" :{
             "key": "a-1e4plt-muh1l3uuyj",
             "token": "GNZR+UcZgkiy*3W0z)"
             }
-}
-clientMDB = MongoClient("mongodb+srv://vutrantienbao290699:vutrantienbao99@project.murnk.mongodb.net/ibm?retryWrites=true&w=majority")
-# Khởi tạo một kết nối
-client = wiotp.sdk.application.ApplicationClient(config=myConfig)
+    }
+    clientMDB = MongoClient("mongodb+srv://vutrantienbao290699:vutrantienbao99@project.murnk.mongodb.net/ibm?retryWrites=true&w=majority")
+    # Khởi tạo một kết nối
+    client = wiotp.sdk.application.ApplicationClient(config=myConfig)
 
-# Kết nối
-client.connect()
-db = clientMDB["ibm"]
-mycol = db.record
+    # Kết nối
+    client.connect()
+    db = clientMDB["ibm"]
+    mycol = db.record
 
-# Sự kiện khi thiết bị gởi lên server
-def myEventCallback(event):
-    str = "%s event '%s' received from device [%s]: %s"
-    print(json.dumps(event.data))
-    mycol.insert_one({"data":json.dumps(event.data)})
-# Gán sự kiện
-client.deviceEventCallback = myEventCallback
+    # Sự kiện khi thiết bị gởi lên server
+    def myEventCallback(event):
+        str = "%s event '%s' received from device [%s]: %s"
+        print(json.dumps(event.data))
+        mycol.insert_one({"data":json.dumps(event.data)})
+    # Gán sự kiện
+    client.deviceEventCallback = myEventCallback
 
-# Liên tục theo dõi  các sự kiện từ thiết bị
-while True:
-    client.subscribeToDeviceEvents()
+    # Liên tục theo dõi  các sự kiện từ thiết bị
+    while True:
+        client.subscribeToDeviceEvents()
+    return "Welcome!"
+if __name__ == '__main__':
+    app.run()
+
